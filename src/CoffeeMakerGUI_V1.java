caedc7862f80e344943ab70ddefe51cfb1ac4889
Keegan Murphy <Keegater0576@unm.edu>
2024-09-26 15:13:45 -0600

    Added Controller + some testing logic

--- src/CoffeeMakerGUI_V1.java
+++ src/CoffeeMakerGUI_V1.java
@@ -9,17 +9,20 @@ public class CoffeeMakerGUI_V1 extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Load the FXML file
-        Parent root = FXMLLoader.load(getClass().getResource("/SceneBuilder Exports/v1.fxml"));
 
-        // Create the scene with the loaded layout
-        Scene scene = new Scene(root);
-        // Set the title and add the scene to the stage
-        primaryStage.setTitle("Coffee Maker GUI Demo");
-        primaryStage.setScene(scene);
+            // Load the FXML file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Scenebuilder/CoffeeMaker GUI Demo Scene builder v1.fxml"));
+            Parent root = loader.load();
 
-        // Show the stage
-        primaryStage.show();
+            // Create the scene with the loaded layout
+            Scene scene = new Scene(root);
+            // Set the title and add the scene to the stage
+            primaryStage.setTitle("Coffee Maker GUI Demo");
+            primaryStage.setScene(scene);
+
+            // Show the stage
+            primaryStage.show();
+
     }
 
     public static void main(String[] args) {
---
+++ src/Controller.java
@@ -0,0 +1,51 @@
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Controller {
+
+    @FXML
+    private ImageView sizeIcon, strengthIcon, brewIcon, size1Icon, size2Icon, size3Icon, size4Icon, strength1Icon, strength2Icon, strength3Icon, brewingProgressIcon = null;
+
+    @FXML
+    public void initialize() {
+
+        /** TODO: Create init function
+         * Should load each respective graphic into its imageview
+         * Set all imageviews except those on the home page, to invisible.
+         * Initialize the 'cursor' position on the first item
+         */
+
+
+        // Load testing image
+        Image testImage = new Image(getClass().getResourceAsStream("/Icons/testing_Icon.png"));
+
+        // Set the image for each ImageView
+        sizeIcon.setImage(testImage);
+        strengthIcon.setImage(testImage);
+        brewIcon.setImage(testImage);
+        size1Icon.setImage(testImage);
+        size2Icon.setImage(testImage);
+        size3Icon.setImage(testImage);
+        size4Icon.setImage(testImage);
+        strength1Icon.setImage(testImage);
+        strength2Icon.setImage(testImage);
+        strength3Icon.setImage(testImage);
+        brewingProgressIcon.setImage(testImage);
+
+
+    }
+
+    /**TODO: changePage() is called when a valid selection imput is pressed.
+        Makes all imageviews transparent except on the target page.
+        Updates all internal logic to reflect being on a new page.
+
+     Will probably also need to set the new 'default' selected item (dotted line thingy) on the new page.
+
+     */
+    public void changePage (String page){
+
+    }
+
+
+}
